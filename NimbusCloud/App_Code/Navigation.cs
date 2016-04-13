d1923494581f3cab8692038c674bf96d5774b76f
William (Tanner) Lewis <tanner.lewis@ttu.edu>
2016-04-13 17:26:04 -0500

    Added Drive API resources and navigation class

---
+++ NimbusCloud/App_Code/Navigation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+
+namespace ConsoleApplication1
+{
+    class Navigation
+    {
+        private DriveService service;
+
+        public Navigation(DriveService service)
+        {
+            this.service = service;
+        }
+
+        public IList<File> Navigate(string parent)
+        {
+            string search, s1 = "'", s2 = "' in parents";
+
+            search = s1 + parent + s2;
+
+            return this.GetFiles(search);
+        }
+
+        private IList<File> GetFiles(string search)
+        {
+            FilesResource.ListRequest listRequest = this.service.Files.List();
+
+            if (search != null)
+            {
+                listRequest.Q = search;
+            }
+            else
+            {
+                listRequest.Q = "'root' in parents";
+            }
+
+            IList<File> files = listRequest.Execute().Files;
+
+            return files;
+        }
+    }
+}
