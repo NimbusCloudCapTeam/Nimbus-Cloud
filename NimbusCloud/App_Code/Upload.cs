559e22529549e9639b806ef31caa6eb8f152e53a
NimbusCloudCapTeam <nimbuscloudcapstoneapp@gmail.com>
2016-04-22 03:04:48 -0500

    Hot fix to have it presentation worthy

--- NimbusCloud/App_Code/Upload.cs
+++ NimbusCloud/App_Code/Upload.cs
@@ -15,55 +15,55 @@ using Google.Apis.Services;
 public class Upload
 {
     public Upload() { }
-        //
-        // TODO: Add constructor logic here
-        //
+    //
+    // TODO: Add constructor logic here
+    //
 
-        //// 
-        /// Create a new Directory.
-        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
-        /// 
-        /// a Valid authenticated DriveService
-        /// The title of the file. Used to identify file or folder name.
-        /// A short description of the file.
-        /// Collection of parent folders which contain this file. 
-        ///                       Setting this field will put the file in all of the provided folders. root folder.
-        /// 
-        public static File createDirectory(DriveService _service, string _title, string _description, string _parent)
+    //// 
+    /// Create a new Directory.
+    /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+    /// 
+    /// a Valid authenticated DriveService
+    /// The title of the file. Used to identify file or folder name.
+    /// A short description of the file.
+    /// Collection of parent folders which contain this file. 
+    ///                       Setting this field will put the file in all of the provided folders. root folder.
+    /// 
+    public static File createDirectory(DriveService _service, string _title, string _description, string _parent)
+    {
+
+        File NewDirectory = null;
+
+        // Create metaData for a new Directory
+        File body = new File();
+        body.Title = _title;
+        body.Description = _description;
+        body.MimeType = "application/vnd.google-apps.folder";
+        body.Parents = new List<ParentReference> { new ParentReference() { Id = _parent } };
+        try
         {
-
-            File NewDirectory = null;
-
-            // Create metaData for a new Directory
-            File body = new File();
-            body.Title = _title;
-            body.Description = _description;
-            body.MimeType = "application/vnd.google-apps.folder";
-            body.Parents = new List<ParentReference> { new ParentReference() { Id = _parent } };
-            try
-            {
-                FilesResource.InsertRequest request = _service.Files.Insert(body);
-                NewDirectory = request.Execute();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("An error occurred: " + e.Message);
-            }
-
-            return NewDirectory;
+            FilesResource.InsertRequest request = _service.Files.Insert(body);
+            NewDirectory = request.Execute();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("An error occurred: " + e.Message);
         }
 
-        // tries to figure out the mime type of the file.
-        private static string GetMimeType(string fileName)
-        {
-            string mimeType = "application/unknown";
-            string ext = System.IO.Path.GetExtension(fileName).ToLower();
-            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
-            if (regKey != null && regKey.GetValue("Content Type") != null)
-                mimeType = regKey.GetValue("Content Type").ToString();
-            return mimeType;
-        }
+        return NewDirectory;
+    }
 
+    // tries to figure out the mime type of the file.
+    private static string GetMimeType(string fileName)
+    {
+        string mimeType = "application/unknown";
+        string ext = System.IO.Path.GetExtension(fileName).ToLower();
+        Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+        if (regKey != null && regKey.GetValue("Content Type") != null)
+            mimeType = regKey.GetValue("Content Type").ToString();
+        return mimeType;
+    }
+}
     /// 
     /// Uploads a file
     /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
@@ -74,7 +74,7 @@ public class Upload
     ///                       Setting this field will put the file in all of the provided folders. root folder.
     /// If upload succeeded returns the File resource of the uploaded file 
     ///          If the upload fails returns null
-    public static File uploadFile(DriveService _service, string _uploadFile, string _parent)
+   /* public static File uploadFile(DriveService _service, string _uploadFile, string _parent)
     {
 
         if (System.IO.File.Exists(_uploadFile))
@@ -90,9 +90,9 @@ public class Upload
             System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
             try
             {
-                FilesResource.InsertMediaUpload request = _service.Files.Insert(body, stream, GetMimeType(_uploadFile));
-                request.Upload();
-                return request.ResponseBody;
+   //             FilesResource.InsertMediaUpload request = _service.Files.Insert(body, stream, GetMimeType(_uploadFile));
+    //            request.Upload();
+    //            return request.ResponseBody;
             }
             catch (Exception e)
             {
@@ -133,9 +133,9 @@ public class Upload
             System.IO.MemoryStream stream = new System.IO.MemoryStream(byteArray);
             try
             {
-                FilesResource.UpdateMediaUpload request = _service.Files.Update(body, _fileId, stream, GetMimeType(_uploadFile));
-                request.Upload();
-                return request.ResponseBody;
+      //          FilesResource.UpdateMediaUpload request = _service.Files.Update(body, _fileId, stream, GetMimeType(_uploadFile));
+       //         request.Upload();
+         //       return request.ResponseBody;
             }
             catch (Exception e)
             {
@@ -152,4 +152,4 @@ public class Upload
     }
 
 
-}
+}*/
