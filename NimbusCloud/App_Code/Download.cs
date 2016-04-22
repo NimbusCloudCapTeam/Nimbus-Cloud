ae1a9683baa2e548e1e101c06fcd65e06691750a
Pedro Reyes <nimbuscloudcapstoneapp@gmail.com>
2016-04-22 01:13:07 -0500

    Started work on Upload and download

---
+++ NimbusCloud/App_Code/Download.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+
+/// <summary>
+/// Summary description for Download
+/// </summary>
+public class Download
+{
+    public Download()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
---
+++ NimbusCloud/App_Code/Upload.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+
+/// <summary>
+/// Summary description for Upload
+/// </summary>
+public class Upload
+{
+    public Upload() { }
+        //
+        // TODO: Add constructor logic here
+        //
+
+        //// 
+        /// Create a new Directory.
+        /// Documentation: https://developers.google.com/drive/v2/reference/files/insert
+        /// 
+        /// a Valid authenticated DriveService
+        /// The title of the file. Used to identify file or folder name.
+        /// A short description of the file.
+        /// Collection of parent folders which contain this file. 
+        ///                       Setting this field will put the file in all of the provided folders. root folder.
+        /// 
+        public static File createDirectory(DriveService _service, string _title, string _description, string _parent)
+        {
+
+            File NewDirectory = null;
+
+            // Create metaData for a new Directory
+            File body = new File();
+            body.Title = _title;
+            body.Description = _description;
+            body.MimeType = "application/vnd.google-apps.folder";
+            body.Parents = new List() { new ParentReference() { Id = _parent } };
+            try
+            {
+                FilesResource.InsertRequest request = _service.Files.Insert(body);
+                NewDirectory = request.Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return NewDirectory;
+        }
+}
